c7e713578e791c36b65ea30869539a4efe241970
RFAlves2022 <rafaalves1337@gmail.com>
2022-02-13 19:03:34 -0300

    realização dos exercícios 17 e 18 .

---
+++ 018/script.js
@@ -0,0 +1,11 @@
+let n1,n2,media
+console.log("Digite sua primeira nota")
+n1=7
+console.log("Digite sua segunda nota")
+n2=4
+media=(n1+n2)/2
+if(media >= 5){
+    console.log("Você está APROVADO!")
+} else{
+    console.log("VOcê está REPROVADO!")
+}
