abc1b361d2464b848a38f5760d1f7b201ea4ef90
RFAlves2022 <94083934+RFAlves2022@users.noreply.github.com>
2022-03-03 20:15:12 -0300

    criação da interface do exercício 1

--- 001/script.js
+++ 001/script.js
@@ -1,5 +1,13 @@
 function alo(){
-    console.log("Alo Mundo")
-}
+    /*
+    Saída de dados no corpo do navegador:
+    document.write("Alô mundo")
 
-alo()
+    em um div , span, h1, etc:
+    document.quarySelector("#resultado").innerHTML = "Alô mundo"
+
+    em um campo de texto:
+    document.querySelector("#resultado").value = "Alô mundo"
+    */
+    document.querySelector("#resultado").value = "<strong>Alô mundo</strong>"
+}
--- 002/script.js
+++ 002/script.js
@@ -1,5 +1,6 @@
 //Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
 function exibir(n1){
+    console.log("Informe um número!")
     console.log("O número informado foi",n1)
 }
 
